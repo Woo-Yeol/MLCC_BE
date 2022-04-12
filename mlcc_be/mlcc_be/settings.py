36fe10b0df8893424e3a09261bf407e3e1f9e9ca
Kuhy <kwh1019_@naver.com>
2022-04-13 00:31:21 +0900

    우헌::celery관련 설정 추가

--- mlcc_be/mlcc_be/settings.py
+++ mlcc_be/mlcc_be/settings.py
@@ -191,3 +191,10 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
