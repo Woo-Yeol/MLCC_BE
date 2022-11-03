7d1563091214acd75646e4e5657476e0196c8f06
Kuhy <kwh1019_@naver.com>
2022-11-03 14:13:49 +0900

    Update mlcc_django.py

--- mlcc_django.py
+++ mlcc_django.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from multiprocessing.spawn import import_main_path
 import os
 import os.path as osp
 import time
@@ -13,30 +14,38 @@ from mmcv import Config
 from mlcc_systemkits.mlcc_system import MLCC_SYSTEM
 from mlcc_systemkits.utils import load_img_paths, NumpyEncoder
 
-def auto_run_model(date, pc_name, thr):
+self_train_results = []
+
+def auto_run_model(seg_cp_pth, date, pc_name, thr):
+    if seg_cp_pth == 'None':
+        seg_cp_pth = 'D:/work_dir/seg/1year+2year_segmentation_edit/iter_20000.pth'
     args = {
-        'det_cfg': 'C:/Users/user/Desktop/IITP/mmcv_laminate_alignment_system/work_dirs/det/mask_rcnn_r50_fpn_1x_mlcc_refine/mask_rcnn_r50_fpn_1x_mlcc_refine.py',
-        'det_cp': 'C:/Users/user/Desktop/IITP/mmcv_laminate_alignment_system/work_dirs/det/mask_rcnn_r50_fpn_2x_mlcc_refine/latest.pth' ,
-        'seg_cfg': 'C:/Users/user/Desktop/IITP/mmcv_laminate_alignment_system/work_dirs/seg/mlcc_sementic_1k_refine/mlcc_sementic_1k_refine.py',
-        'seg_cp': 'C:/Users/user/Desktop/IITP/mmcv_laminate_alignment_system/work_dirs/seg/mlcc_sementic_1k_refine/latest.pth',
-        'source': f'C:/Users/user/Desktop/IITP/mmcv_laminate_alignment_system/mlcc_datasets/smb/{str(pc_name)}/',
+        # 'det_cfg': 'D:/work_dir/det/1year+2year_detection_edit/detection.py',
+        # 'det_cp': 'D:/work_dir/det/1year+2year_detection_edit/epoch_12.pth' ,
+        # 'seg_cfg': 'D:/work_dir/seg/default_model/mlcc_sementic_1k_refine_small_aug_ssb.py',
+        # 'seg_cp': seg_cp_pth + "/latest.pth",
+        'det_cfg': 'D:/work_dir/det/1year+2year_detection_edit/detection.py',
+        'det_cp': 'D:/work_dir/det/1year+2year_detection_edit/epoch_12.pth' ,
+        'seg_cfg': 'D:/work_dir/seg/1year+2year_segmentation_edit/mlcc_sementic_1k_refine_small_aug_ssb.py',
+        'seg_cp': 'D:/work_dir/seg/1year+2year_segmentation_edit/iter_20000.pth',
+        'source': f'D:/mlcc_datasets/smb/{str(pc_name)}/input/',
         'gpu_id': '0',
         'score_thr': 0.30,
         'remove_edge_bbox': True,
         'refine_ratio': 0.06,
         'margin_thr': thr,
         'cut_off': 0,
+        'rescale': True,
         'name': date,
         'project': 'mmcl_results'
     }   
-    
     det_cfg = Config.fromfile(args['det_cfg'])
     seg_cfg = Config.fromfile(args['seg_cfg'])    
     if args['name'] is None:
         args['name'] = osp.splitext(osp.basename(args.det_cfg))[0]
     save_dir = f"C:/Users/user/Desktop/IITP/mmcv_laminate_alignment_system/{args['project']}/{args['name']}"
     mmcv.mkdir_or_exist(osp.abspath(save_dir))    
-        
+
     det_cfg.dump(osp.join(save_dir, osp.basename(args['det_cfg'])))
     seg_cfg.dump(osp.join(save_dir, osp.basename(args['seg_cfg'])))
     
@@ -49,8 +58,9 @@ def auto_run_model(date, pc_name, thr):
         seg_cfg = args['seg_cfg'],
         seg_cp = args['seg_cp'],
         device=device)
-
+        
     img_paths = load_img_paths(args['source'])
+
     num_inctances = 0
     num_img = 0
     results = []
@@ -61,10 +71,15 @@ def auto_run_model(date, pc_name, thr):
                                          remove_edge_bbox = args['remove_edge_bbox'],
                                          refine_ratio = args['refine_ratio'],
                                          margin_thr = args['margin_thr'],
+                                         rescale = args['rescale'],
                                          cut_off = args['cut_off'])
-        results.append(result)
+        if not result['bboxes']:
+            a = cv2.imread(img_path)
+            cv2.imwrite(('D:\mlcc_datasets\smb\data\_failed'+'\\'+result['img_basename']), a)
+            continue
+        else: 
+            yield result
         #mlcc_system.save_results(result, save_dir)         
-    return results
 
 
 def manual_run_model(pc_name, thr):
@@ -83,7 +98,6 @@ def manual_run_model(pc_name, thr):
         'name': '',
         'project': 'mmcl_results'
     }   
-    
     det_cfg = Config.fromfile(args['det_cfg'])
     seg_cfg = Config.fromfile(args['seg_cfg'])    
     if args['name'] is None:
